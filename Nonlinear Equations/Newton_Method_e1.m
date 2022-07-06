cf7a17570f71f3aeb97684325cf12ec195d2b000
Nha Do <nhado401@gmail.com>
2022-07-05 21:19:59 -0400

    Create Newton_Method_e1.m

---
+++ Nonlinear Equations/Newton_Method_e1.m
@@ -0,0 +1,36 @@
+clc;
+clear all;
+close all;
+p0 = 0.5; %initial condition
+N = 100; %maximum number of iteration
+k = 1;
+error = 10^-8; %value to terminate iteration
+syms 'x';
+f(x) = x^3 - 6*x^2 + 11*x - 5; %function
+d_f = diff(f); %derivative
+iter = [];
+root = [];
+while k < N && abs(f(p0)) > error
+   p = p0 - (f(p0)/d_f(p0)); %calculate the value
+   iter = [iter, k];
+   root = [root, round(p,4)];
+   k = k + 1;
+   p0 = p; %update the value
+end
+result = round(vpa(p0),4) %root
+figure(1)
+scatter(iter,root,'LineWidth',1);
+hold on
+plot(iter,root,'LineWidth',1);
+xlabel("k");
+ylabel("f(x(k))");
+title("Speed of Convergence");
+grid on;
+t = 0:0.1:4;
+figure(2)
+f_t = t.^3 - 6*t.^2 + 10*t;
+plot(t,f_t,'LineWidth',1);
+hold on
+f_hat = -t + 5;
+plot(t,f_hat,'LineWidth',1);
+grid on;
